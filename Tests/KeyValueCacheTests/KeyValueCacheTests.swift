c746cc3de4404385e08a6f0b63ebc44b278dc0f1
vitali-kurlovich <vitalikurlovich@gmail.com>
2022-06-02 17:02:25 +0300

    Update KeyValueCacheTests.swift

--- Tests/KeyValueCacheTests/KeyValueCacheTests.swift
+++ Tests/KeyValueCacheTests/KeyValueCacheTests.swift
@@ -2,7 +2,7 @@
 import XCTest
 
 final class KeyValueCacheTests: XCTestCase {
-    func testKeyValueCache() throws {
+    func testExpire() throws {
         let valueCache = KeyValueCache<String, String>()
 
         let now = Date()
@@ -10,6 +10,8 @@ final class KeyValueCacheTests: XCTestCase {
         valueCache.setValue("AAAAAA", forKey: "A", expireDate: now.addingTimeInterval(3))
 
         valueCache.setValue("BBBBBB", forKey: "B", expireDate: now.addingTimeInterval(6))
+
+        valueCache.setValue("CCCCCC", forKey: "C")
 
         let expA = XCTestExpectation(description: "Read for 'A' key")
 
@@ -64,6 +66,190 @@ final class KeyValueCacheTests: XCTestCase {
             expB2.fulfill()
         }
 
-        wait(for: [expA2, expB2], timeout: 1.0)
+        let expC = XCTestExpectation(description: "Read for 'C'")
+
+        valueCache.value(forKey: "C").whenSuccess { value in
+            XCTAssertEqual("CCCCCC", value)
+
+            expC.fulfill()
+        }
+
+        wait(for: [expA2, expB2, expC], timeout: 1.0)
+    }
+
+    func testRemoveValue() throws {
+        let valueCache = KeyValueCache<String, String>()
+
+        let now = Date()
+
+        valueCache.setValue("AAAAAA", forKey: "A", expireDate: now.addingTimeInterval(3))
+
+        valueCache.setValue("BBBBBB", forKey: "B", expireDate: now.addingTimeInterval(6))
+
+        valueCache.setValue("CCCCCC", forKey: "C")
+
+        var expA = XCTestExpectation(description: "Read for 'A' key")
+        var expB = XCTestExpectation(description: "Read for 'B' key")
+        var expC = XCTestExpectation(description: "Read for 'C' key")
+
+        valueCache.value(forKey: "A").whenSuccess { value in
+
+            XCTAssertEqual("AAAAAA", value)
+            expA.fulfill()
+        }
+
+        valueCache.value(forKey: "B").whenSuccess { value in
+
+            XCTAssertEqual("BBBBBB", value)
+            expB.fulfill()
+        }
+
+        valueCache.value(forKey: "C").whenSuccess { value in
+
+            XCTAssertEqual("CCCCCC", value)
+            expC.fulfill()
+        }
+
+        wait(for: [expA, expB, expC], timeout: 1.0)
+
+        expA = XCTestExpectation(description: "Read for 'A' key")
+        expB = XCTestExpectation(description: "Read for 'B' key")
+        expC = XCTestExpectation(description: "Read for 'C' key")
+
+        valueCache.removeValue(forKey: "A")
+
+        valueCache.value(forKey: "A").whenSuccess { value in
+
+            XCTAssertNil(value)
+            expA.fulfill()
+        }
+
+        valueCache.value(forKey: "B").whenSuccess { value in
+
+            XCTAssertEqual("BBBBBB", value)
+            expB.fulfill()
+        }
+
+        valueCache.value(forKey: "C").whenSuccess { value in
+
+            XCTAssertEqual("CCCCCC", value)
+            expC.fulfill()
+        }
+
+        wait(for: [expA, expB, expC], timeout: 1.0)
+
+        expA = XCTestExpectation(description: "Read for 'A' key")
+        expB = XCTestExpectation(description: "Read for 'B' key")
+        expC = XCTestExpectation(description: "Read for 'C' key")
+
+        valueCache.removeValue(forKey: "C")
+
+        valueCache.value(forKey: "A").whenSuccess { value in
+
+            XCTAssertNil(value)
+            expA.fulfill()
+        }
+
+        valueCache.value(forKey: "B").whenSuccess { value in
+
+            XCTAssertEqual("BBBBBB", value)
+            expB.fulfill()
+        }
+
+        valueCache.value(forKey: "C").whenSuccess { value in
+
+            XCTAssertNil(value)
+            expC.fulfill()
+        }
+
+        wait(for: [expA, expB, expC], timeout: 1.0)
+
+        expA = XCTestExpectation(description: "Read for 'A' key")
+        expB = XCTestExpectation(description: "Read for 'B' key")
+        expC = XCTestExpectation(description: "Read for 'C' key")
+
+        valueCache.removeValue(forKey: "B")
+
+        valueCache.value(forKey: "A").whenSuccess { value in
+
+            XCTAssertNil(value)
+            expA.fulfill()
+        }
+
+        valueCache.value(forKey: "B").whenSuccess { value in
+
+            XCTAssertNil(value)
+            expB.fulfill()
+        }
+
+        valueCache.value(forKey: "C").whenSuccess { value in
+
+            XCTAssertNil(value)
+            expC.fulfill()
+        }
+
+        wait(for: [expA, expB, expC], timeout: 1.0)
+    }
+
+    func testRemoveAll() throws {
+        let valueCache = KeyValueCache<String, String>()
+
+        let now = Date()
+
+        valueCache.setValue("AAAAAA", forKey: "A", expireDate: now.addingTimeInterval(3))
+
+        valueCache.setValue("BBBBBB", forKey: "B", expireDate: now.addingTimeInterval(6))
+
+        valueCache.setValue("CCCCCC", forKey: "C")
+
+        var expA = XCTestExpectation(description: "Read for 'A' key")
+        var expB = XCTestExpectation(description: "Read for 'B' key")
+        var expC = XCTestExpectation(description: "Read for 'C' key")
+
+        valueCache.value(forKey: "A").whenSuccess { value in
+
+            XCTAssertEqual("AAAAAA", value)
+            expA.fulfill()
+        }
+
+        valueCache.value(forKey: "B").whenSuccess { value in
+
+            XCTAssertEqual("BBBBBB", value)
+            expB.fulfill()
+        }
+
+        valueCache.value(forKey: "C").whenSuccess { value in
+
+            XCTAssertEqual("CCCCCC", value)
+            expC.fulfill()
+        }
+
+        wait(for: [expA, expB, expC], timeout: 1.0)
+
+        expA = XCTestExpectation(description: "Read for 'A' key")
+        expB = XCTestExpectation(description: "Read for 'B' key")
+        expC = XCTestExpectation(description: "Read for 'C' key")
+
+        valueCache.removeAllValues()
+
+        valueCache.value(forKey: "A").whenSuccess { value in
+
+            XCTAssertNil(value)
+            expA.fulfill()
+        }
+
+        valueCache.value(forKey: "B").whenSuccess { value in
+
+            XCTAssertNil(value)
+            expB.fulfill()
+        }
+
+        valueCache.value(forKey: "C").whenSuccess { value in
+
+            XCTAssertNil(value)
+            expC.fulfill()
+        }
+
+        wait(for: [expA, expB, expC], timeout: 1.0)
     }
 }
